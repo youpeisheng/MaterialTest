c1f232ac2c8a53513cfcd121f8a0649c260755b3
youpeisheng <1426890389@qq.com>
2019-12-12 16:26:11 +0800

    添加了按钮按下提示

--- app/src/main/java/com/example/materialtest/MainActivity.java
+++ app/src/main/java/com/example/materialtest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.materialtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
+            case R.id.settings:
+                Toast.makeText(this,"You clicked Settings",Toast.LENGTH_SHORT).show();
+                default:
+                    break;
+        }
+        return true;
     }
 }
