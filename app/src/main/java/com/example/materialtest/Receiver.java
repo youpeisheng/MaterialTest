138266d0d2d602654a18f0974d241b0477ff7e5f
youpeisheng <1426890389@qq.com>
2019-12-15 11:15:32 +0800

    新增 开机自启动应用设置

---
+++ app/src/main/java/com/example/materialtest/Receiver.java
@@ -0,0 +1,21 @@
+package com.example.materialtest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.e("broadCastReceiver","onReceiver...");
+        try {
+            Intent mBootIntent = new Intent(context, MainActivity.class);
+            // 下面这句话必须加上才能开机自动运行app的界面
+            mBootIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mBootIntent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
